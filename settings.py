2acdc9be721b2ada02a4f92f7e470f07974a91b9
kalinkochnev <kalin.kochnev@gmail.com>
2021-04-24 18:35:25 -0400

    Initial commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+CLASSES = {
+    "Calc": "BC Calculus 20-21",
+    "Gov": "Law & Gov",
+    "Writing": "Professional and Technical Writing",
+    "Physics": "Block5 (mod9) AP Physics C - DeLano",
+}
